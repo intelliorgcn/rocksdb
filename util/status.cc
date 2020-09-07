fe98ddfd7dd6057681bed1970c28c4c9e7435c39
sunyu <sun-yu1987@qq.com>
2020-09-07 22:08:18 +0800

    fix strncpy warning

--- util/status.cc
+++ util/status.cc
@@ -25,7 +25,7 @@ const char* Status::CopyState(const char* state) {
   ret = strncpy_s(result, cch, state, cch - 1);
   assert(ret == 0);
 #else
-  std::strncpy(result, state, cch - 1);
+  std::memcpy(result, state, cch - 1);
 #endif
   return result;
 }
